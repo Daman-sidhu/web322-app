83793e4ac0af30f8bca348a7d19b4a3f2e1750d0
Daman-sidhu <damnsidhu04@gmail.com>
2022-02-21 23:30:46 -0500

    hureh

--- blog-service.js
+++ blog-service.js
@@ -1,56 +1,105 @@
 const fs = require('fs')
 
-let  posts = []
+let posts = []
 let categories = []
 
 module.exports.initialize = () => {
 
-    return new Promise((resolve,reject) => {
-    
-    fs.readFile('./data/posts.json','utf8',(error,data)=> {
-            if(error) {
+    return new Promise((resolve, reject) => {
+
+        fs.readFile('./data/posts.json', 'utf8', (error, data) => {
+            if (error) {
                 reject('unable to open file')
             }
-                
-                posts.push(...JSON.parse(data))
+
+            posts.push(...JSON.parse(data))
         })
-    
-    fs.readFile('./data/categories.json','utf8',(error,data)=> {
-            if(error) {
+
+        fs.readFile('./data/categories.json', 'utf8', (error, data) => {
+            if (error) {
                 reject('unable to open file')
             }
-               
-                categories.push(JSON.parse(data))
+
+            categories.push(JSON.parse(data))
+        })
+        resolve('success')
     })
-    resolve('success')
- })
 }
 
 
 module.exports.getAllPosts = () => {
 
-return new Promise((resolve,reject)=> {
-    if(posts.length !== 0) {
-        resolve(posts)
-    }
-    reject('no results returned')
-  })
+    return new Promise((resolve, reject) => {
+        if (posts.length !== 0) {
+            resolve(posts)
+        }
+        reject('no results returned')
+    })
 }
 module.exports.getPublishedPosts = () => {
     const Published = posts.filter(post => post.published === true)
-return new Promise((resolve,reject)=> {
-    
-    if(Published.length !== 0) {
-     resolve(Published)
-    }
-    reject('no results returned')
-  })
+    return new Promise((resolve, reject) => {
+
+        if (Published.length !== 0) {
+            resolve(Published)
+        }
+        reject('no results returned')
+    })
 }
 module.exports.getCategories = () => {
-return new Promise((resolve,reject) => {
-    if(categories.length !== 0) {
-        resolve(categories)
-    }
-    reject('no results returned')
-   })
+    return new Promise((resolve, reject) => {
+        if (categories.length !== 0) {
+            resolve(categories)
+        }
+        reject('no results returned')
+    })
+}
+module.exports.addpost = (postData) => {
+    return new Promise((resolve, reject) => {
+        if (postData.published) {
+            postData.published = false
+        } else {
+            postData.published = true
+        }
+
+        postData.id = posts.length + 1
+        posts.push(postData)
+        resolve(postData)
+        if (!postData) {
+            reject("Error")
+        }
+    })
+}
+
+module.exports.getPostsByCategory = (category) => {
+    const newPosts = posts.filter(post => post.category == category)
+    return new Promise((resolve,reject)=> {
+        
+        if(newPosts.length !== 0) {
+            resolve(newPosts)
+        }
+        reject('no result returned!')
+    })
+}
+
+module.exports.getPostsbyMinDate = (minDateStr) => {
+    const newPosts = posts.filter(post=> new Date(post.postDate) >= new Date(minDateStr) )
+    return new Promise((resolve,reject)=> {
+        
+        if(newPosts.length !== 0) {
+            resolve(newPosts)
+        }
+        reject('no result returned!')
+    })
+} 
+
+module.exports.getPostById = (id) => {
+    const newPosts = posts.find(post => post.id == id )
+    return new Promise((resolve,reject)=> {
+        
+        if(newPosts) {
+            resolve(newPosts)
+        }
+        reject('no result returned!')
+    })
 }
--- server.js
+++ server.js
@@ -1,11 +1,36 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: ___Damanpreet Singh___________________ Student ID: ____145560207__________ Date: _____5-02-2022___________
+*
+*  Online (Heroku) URL: _______https://dmnsidhu.herokuapp.com/_________________________________________________
+*
+*  GitHub Repository URL: _______________https://github.com/Daman-sidhu/web322-app_______________________________________
+*
+********************************************************************************/ 
+
 const express = require ('express')
 const { lstat } = require('fs')
 const blogService = require('./blog-service')
 const path = require ('path')
 const app = express()
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+const upload = multer();
+
 
 const port = process.env.PORT || 8080
 app.use(express.static(path.join(__dirname,'/public')))
+cloudinary.config({
+    cloud_name: 'dzi6uaysi',
+    api_key: '774313852437156',
+    api_secret: 'rprxymlfIzRGvduQFUNTY0FQGds',
+    secure: true
+});
+
 
 app.get("/", (req,res) => {
     res.redirect("/about");
@@ -26,16 +51,6 @@ app.get('/blog',(req,res)=>{
     })
 })
 
-app.get('/posts',(req,res)=>{
-    blogService.getAllPosts().then((data)=>{
-    res.send(data)
-    }).catch((error)=> {
-    res.send({
-        error: error
-    })
-    
-    })
-})
 
 app.get('/categories',(req,res)=>{
     blogService.getCategories().then((data)=>{
@@ -48,17 +63,93 @@ app.get('/categories',(req,res)=>{
     })
 })
 
+app.get("/posts/add",(req,res)=> {
+    res.sendFile(path.join(__dirname,'/views/addPost.html'))
+})
 
+app.post("/posts/add",upload.single("featureImage"),(req,res)=> {
+    let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+            let stream = cloudinary.uploader.upload_stream(
+                (error, result) => {
+                if (result) {
+                    resolve(result);
+                } else {
+                    reject(error);
+                }
+                }
+            );
+    
+            streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+    };
+    
+    async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result);
+        return result;
+    }
+    
+    upload(req).then((uploaded)=>{
+        req.body.featureImage = uploaded.url;
+    
+        // TODO: Process the req.body and add it as a new Blog Post before redirecting to /posts
+    blogService.addpost(req.body).then((data)=>{
+      res.redirect('/posts')
+    }).catch((error)=> {
+res.send(error)
+    })
+    });
+    
+})
+
+app.get("/posts", (req, res) => {
+    if (req.query.category) {
+      blogService
+        .getPostsByCategory(req.query.category)
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((error) => {
+          return res.send({
+            error: error,
+          });
+        });
+    } else if (req.query.minDate) {
+      blogService
+        .getPostsbyMinDate(req.query.minDate)
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((error) => {
+          return res.send({
+            error: error,
+          });
+        });
+    } else {
+      blogService
+        .getAllPosts()
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((error) => {
+          return res.send({
+            error: error,
+          });
+        });
+    }
+  });
+
+  app.get('/post/:id',(req,res)=> {
+      blogService.getPostById(req.params.id).then((data)=> {
+          res.send(data)
+      })
+  })
 
 blogService.initialize().then((data)=>{
-
-
-
     app.listen(port, ()=> {
         console.log(`server is currently listening on (ie: 8080`)
     });
 }).catch((error)=> {
     console.log(error)
 })
-
-
